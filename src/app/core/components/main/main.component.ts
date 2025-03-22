058f4b388f613f8e526dbb289642ba67e7bab7cc
tw1ttt3r <tw1ttt3r@gmail.com>
2025-03-21 20:37:13 -0600

    feat: added core module

---
+++ src/app/core/components/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee-main',
+  template: `
+    <employee-image alt="Logo UPT" class="w-[180px] pb-4" src="/logo_upt.png" />
+    <h1 class="text-red-500">...Cargando</h1>
+  `
+})
+export class MainComponent {
+
+}
